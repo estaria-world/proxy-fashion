d8106f29bc1e0bd88fbf6c06f8549de83c096e73
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-06-04 21:28:14 +0200

    remove old tablist config

--- src/main/kotlin/world/estaria/proxy/fashion/config/Config.kt
+++ src/main/kotlin/world/estaria/proxy/fashion/config/Config.kt
@@ -1,7 +1,6 @@
 package world.estaria.proxy.fashion.config
 
 import kotlinx.serialization.Serializable
-import world.estaria.server.manager.api.enums.ServerVariant
 
 /**
  * @author Niklas Nieberler
@@ -10,27 +9,16 @@ import world.estaria.server.manager.api.enums.ServerVariant
 @Serializable
 class Config(
     private val motds: List<FashionMotd>,
-    private val tablists: List<FashionTablist>
 ) {
 
     fun getMotd(type: FashionMotd.Type): FashionMotd {
         return this.motds.first { it.type == type }
     }
 
-    fun getTablist(variant: ServerVariant): FashionTablist? {
-        return this.tablists.firstOrNull { it.variant == variant }
-    }
-
-    fun getDefaultTablist(): FashionTablist {
-        return getTablist(ServerVariant.SERVER)
-            ?: throw NullPointerException("failed to find default tablist")
-    }
-
     object Default {
         fun get(): Config {
             return Config(
-                FashionMotd.Type.entries.map { FashionMotd.Default.get(it) },
-                ServerVariant.entries.map { FashionTablist.Default.get(it) }
+                FashionMotd.Type.entries.map { FashionMotd.Default.get(it) }
             )
         }
     }
